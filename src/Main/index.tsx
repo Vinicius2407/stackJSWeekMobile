170b6c54c6f8831fa9677388ed3de4c62aa3ce2e
Vinícius Silva <viniciussilvapereira24@gmail.com>
2022-12-21 16:38:23 -0300

    feat: made the menu and the modal to add the table number of the order

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -5,10 +5,19 @@ import { Header } from "../components/Header";
 import { Categories } from "../components/Categories";
 import { Menu } from "../components/Menu";
 import { Button } from "../components/Button";
+import { TableModal } from "../components/TableModal";
 
+import { useState } from "react"
 // 2hrs,09min
 
 export function Main() {
+  const [isTableModalVisible, setIsTableModalVisible] = useState(false);
+  const [selectedTable, setSelectedTable] = useState("")
+
+  function handleSaveTable(table: string) {
+    setSelectedTable(table);
+  }
+
   return (
     <>
       <Container>
@@ -25,11 +34,19 @@ export function Main() {
       </Container>
       <Footer>
         <FooterContainer >
-          <Button onPress={() => alert("novo pedido")}>
-            Novo Pedido
-          </Button>
+          {!selectedTable && (
+            <Button onPress={() => setIsTableModalVisible(true)}>
+              Novo Pedido
+            </Button>
+          )}
         </FooterContainer>
       </Footer>
+
+      <TableModal
+        visible={isTableModalVisible}
+        onClose={() => setIsTableModalVisible(false)}
+        onSave={handleSaveTable}
+      />
     </>
 
   )
